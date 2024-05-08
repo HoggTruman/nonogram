52053ea9fab2e6f8cd1581c5b26c9d2b04f75e38
Truman Hogg <hoggtruman@gmail.com>
2024-05-08 22:05:48 +0100

    Added simple Board component tests
    Added tests for whether cells are initially rendered correctly from props (does not test correct behaviour on user interaction)

---
+++ src/components/__tests__/Board.test.js
@@ -0,0 +1,115 @@
+/** @jest-environment jsdom */
+import React from 'react';
+import '@testing-library/jest-dom';
+import {render, fireEvent, screen} from '@testing-library/react';
+
+import Board from "../Board";
+import { CELL_STATE, CELL_STATE_CLASSES } from "../../utility/constants";
+
+const SIZES = [5, 10, 15, 20, 25];
+
+describe("Board Component tests", () => {
+  describe("elements rendered correctly from cells prop", () => {
+    test.each(SIZES)("board renders correct number of cells", size => {
+      const cells = Array.from({length: size}, () => (
+        Array.from({length: size}, () => CELL_STATE.BLANK))
+      );
+      const setCellState = jest.fn(); // unused
+      const puzzleComplete = false; // unused
+    
+      render(
+        <Board 
+          cells={cells}
+          setCellState={setCellState}
+          puzzleComplete={puzzleComplete}
+        />
+      );
+
+      const cellElements = screen.getAllByRole("button");
+
+      expect(cellElements).toHaveLength(size * size);
+      cellElements.forEach(cell => {
+        expect(cell).toHaveClass('cell');
+      });
+    })
+
+
+
+    test("board renders 'BLANK' cells with correct class", () => {
+      const cells = Array.from({length: 5}, () => (
+        Array.from({length: 5}, () => CELL_STATE.BLANK))
+      );
+      const setCellState = jest.fn(); // unused
+      const puzzleComplete = false; // unused
+    
+      render(
+        <Board 
+          cells={cells}
+          setCellState={setCellState}
+          puzzleComplete={puzzleComplete}
+        />
+      );
+
+      const cellElements = screen.getAllByRole("button");
+      cellElements.forEach(cell => {
+        expect(cell).toHaveClass('cell');
+        expect(cell).toHaveClass(CELL_STATE_CLASSES[CELL_STATE.BLANK])
+        expect(cell).not.toHaveClass(CELL_STATE_CLASSES[CELL_STATE.FILLED])
+        expect(cell).not.toHaveClass(CELL_STATE_CLASSES[CELL_STATE.CROSSED])
+      });
+    })
+
+
+
+    test("board renders 'FILLED' cells with correct class", () => {
+      const cells = Array.from({length: 5}, () => (
+        Array.from({length: 5}, () => CELL_STATE.FILLED))
+      );
+      const setCellState = jest.fn(); // unused
+      const puzzleComplete = false; // unused
+    
+      render(
+        <Board 
+          cells={cells}
+          setCellState={setCellState}
+          puzzleComplete={puzzleComplete}
+        />
+      );
+
+      const cellElements = screen.getAllByRole("button");
+      cellElements.forEach(cell => {
+        expect(cell).toHaveClass('cell');
+        expect(cell).toHaveClass(CELL_STATE_CLASSES[CELL_STATE.FILLED])
+        expect(cell).not.toHaveClass(CELL_STATE_CLASSES[CELL_STATE.BLANK])
+        expect(cell).not.toHaveClass(CELL_STATE_CLASSES[CELL_STATE.CROSSED])
+      });
+    });
+
+
+
+    test("board renders 'CROSSED' cells with correct class", () => {
+      const cells = Array.from({length: 5}, () => (
+        Array.from({length: 5}, () => CELL_STATE.CROSSED))
+      );
+      const setCellState = jest.fn(); // unused
+      const puzzleComplete = false; // unused
+    
+      render(
+        <Board 
+          cells={cells}
+          setCellState={setCellState}
+          puzzleComplete={puzzleComplete}
+        />
+      );
+
+      const cellElements = screen.getAllByRole("button");
+      cellElements.forEach(cell => {
+        expect(cell).toHaveClass('cell');
+        expect(cell).toHaveClass(CELL_STATE_CLASSES[CELL_STATE.CROSSED])
+        expect(cell).not.toHaveClass(CELL_STATE_CLASSES[CELL_STATE.BLANK])
+        expect(cell).not.toHaveClass(CELL_STATE_CLASSES[CELL_STATE.FILLED])
+      });
+    });
+  })
+})
+
