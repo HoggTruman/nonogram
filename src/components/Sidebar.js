619087d2a3d46e72117718572e3d1efcd27143e6
Truman Hogg <hoggtruman@gmail.com>
2024-05-20 18:28:43 +0100

    Add Sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,91 @@
+import React from "react";
+import { BOARD_SIZES } from "../utility/constants";
+
+import "./styles/Sidebar.css";
+import logo from "../assets/logo.jpg";
+
+
+
+class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    const generatePuzzleButton = document.getElementById("generate-from-seed");
+
+    generatePuzzleButton.addEventListener('click', event => {
+      event.preventDefault();
+
+      const formData = new FormData(document.getElementById("puzzle-form")); 
+      const size = formData.get('puzzle-size');
+      const seed = formData.get('seed') || Math.floor(Math.random() * 1000000000);
+      
+      this.props.navigateToNewPuzzlePage(size, seed)
+    })
+  }
+  
+
+
+  render() {
+    const sizeRadioButtons = BOARD_SIZES.map(BOARD_SIZE => (
+      <div key={BOARD_SIZE} className="size-button">
+        <input 
+          type="radio" 
+          className="btn-check" 
+          id={`radio-size-${BOARD_SIZE}`} 
+          value={BOARD_SIZE} 
+          name="puzzle-size" 
+          autoComplete="off" 
+          defaultChecked={Number(this.props.size) === BOARD_SIZE}
+        />
+        <label 
+          className="btn" 
+          htmlFor={`radio-size-${BOARD_SIZE}`} 
+        >
+          {`${BOARD_SIZE} x ${BOARD_SIZE}`} 
+        </label>
+      </div>
+    ))
+
+
+    return (
+      <div id="sidebar">
+        <img src={logo} id="logo" alt="logo" title="Go to Homepage" />
+
+        <hr className="thick"/>
+        <h2>New Puzzle</h2>
+        <hr className="thick"/>
+
+        <form id="puzzle-form">
+          <div id="size-buttons">
+            {sizeRadioButtons}
+          </div>
+
+          <div id="enter-seed">
+            <label htmlFor="seed">Enter seed (or leave blank for random):</label>
+            <input type="number" id="seed" name="seed" />
+          </div>
+
+          <input
+            type="submit"
+            id="generate-from-seed"
+            value="Generate Puzzle"
+          />
+        </form>
+
+        <hr className="thick"/>
+
+        <input
+            type="button"
+            id="how-to-play"
+            value="How to play"
+        />
+
+      </div>
+    )
+  }
+
+}
+
+export default Sidebar;
--- src/pages/PuzzlePage.js
+++ src/pages/PuzzlePage.js
@@ -5,11 +5,15 @@ import Puzzle from "../components/Puzzle";
 import PuzzleMenu from "../components/PuzzleMenu";
 import generatePuzzle from "../utility/generatePuzzle";
 import generateDefaultCells from "../utility/generateDefaultCells";
-//import "./PuzzlePage.css";
+import SideBar from "../components/SideBar";
+import WithNavigateHook from "../components/WithNavigateHook";
+
+import "./styles/PuzzlePage.css";
 
 
-function PuzzlePage() {
-  let {size, seed} = useParams();
+
+function PuzzlePage(props) {
+  const {size, seed} = useParams();  // should rename to avoid confusion???????????????????????????????????????????????????????????
 
   // default state
   const defaultCells = generateDefaultCells(size);
@@ -17,31 +21,46 @@ function PuzzlePage() {
   // state 
   const [cells, setCells] = useState(defaultCells);
   const [puzzleComplete, setPuzzleComplete] = useState(false);
+  console.log(cells);
 
   const solution = generatePuzzle(size, seed);  
+
+  function navigateToNewPuzzlePage(size, seed) {
+    const newUrl = /*'puzzle' +*/ '/' + size + '/' + seed
+    props.navigation(newUrl)
+    setCells(generateDefaultCells(size));
+    setPuzzleComplete(false);  // use null instead of false initially to correspond to no message displayed?
+  }
     
   return (
     <>
-      <h1>Size: {size}</h1>
-      <h1>Seed: {seed}</h1>
-      <Puzzle 
+      <SideBar
         size={size}
-        seed={seed}
-        cells={cells}
-        setCells={setCells}
-        puzzleComplete={puzzleComplete}
-        solution={solution}
+        navigateToNewPuzzlePage={navigateToNewPuzzlePage}
       />
-      <PuzzleMenu
-        cells={cells}
-        setCells={setCells}
-        puzzleComplete={puzzleComplete}
-        setPuzzleComplete={setPuzzleComplete}
-        defaultCells={defaultCells}
-        solution={solution}
-      />
+      <main>
+        <h1>Size: {size}</h1>
+        <h1>Seed: {seed}</h1>
+        <Puzzle 
+          size={size}
+          seed={seed}
+          cells={cells}
+          setCells={setCells}
+          puzzleComplete={puzzleComplete}
+          solution={solution}
+        />
+        <PuzzleMenu
+          cells={cells}
+          setCells={setCells}
+          puzzleComplete={puzzleComplete}
+          setPuzzleComplete={setPuzzleComplete}
+          defaultCells={defaultCells}
+          solution={solution}
+        />
+      </main>
+      
     </>
   )
 }
 
-export default PuzzlePage;
+export default WithNavigateHook(PuzzlePage);
