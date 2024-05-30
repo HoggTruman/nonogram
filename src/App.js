b93c22637d4d1f55a186c93db3d8f03c1e1fcd04
Truman Hogg <hoggtruman@gmail.com>
2024-05-30 01:26:50 +0100

    Remove unnecessary exact attribute in HomePage Route

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends React.Component {
     return (
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={ <HomePage /> } />
+          <Route path="/" element={ <HomePage /> } />
           <Route path="/puzzle/:size/:seed" element={ <PuzzlePage /> } />
           <Route path="*" element={ <NotFoundPage />} />
         </Routes>
